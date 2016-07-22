52b7e08af74119fd1d74e5a2a315816989a2cfee
danik <me@subsonic.cz>
2016-07-22 19:51:34 +0200

    fix parent constructor call in DI bridge

--- dist/js/nittro-extras-paginator.js
+++ dist/js/nittro-extras-paginator.js
@@ -410,7 +410,7 @@ _context.invoke('Nittro.Extras.Paginator.Bridges', function(Nittro) {
     }
 
     var PaginatorDI = _context.extend('Nittro.DI.BuilderExtension', function(containerBuilder, config) {
-        PaginatorDI.Super.call(containerBuilder, config);
+        PaginatorDI.Super.call(this, containerBuilder, config);
     }, {
         load: function() {
             this._getContainerBuilder().addFactory('paginator', 'Nittro.Extras.Paginator.Paginator()');
